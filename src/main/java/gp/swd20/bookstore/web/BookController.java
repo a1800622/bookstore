7a566e52006301bc6dd0c43411b0a7e532ed1b2b
Georg Pärn <a1800622@myy.haaga-helia.fi>
2019-03-11 16:09:30 +0200

    new commit REST

--- src/main/java/gp/swd20/bookstore/web/BookController.java
+++ src/main/java/gp/swd20/bookstore/web/BookController.java
@@ -1,12 +1,17 @@
 package gp.swd20.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import gp.swd20.bookstore.domain.Book;
 import gp.swd20.bookstore.domain.BookRepository;
@@ -27,6 +32,25 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist"; //booklist.html
 	}
+	
+	//REST kaikki kirjat books
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) repository.findAll();
+	} 
+	
+	//REST kirja id:llä
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+		return repository.findById(bookId);
+	}
+	
+	//REST lisää kirja
+	@RequestMapping(value="books", method = RequestMethod.POST)
+	public @ResponseBody Book addNewBookRest(@RequestBody Book book) {
+		return repository.save(book);
+	}
+	
 	
 	//tyhjä lomake uudesta kirjasta
 	@RequestMapping(value="/add")
